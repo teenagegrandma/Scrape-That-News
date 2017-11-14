e6c9a1d5364add639ffe1fc3ed163a2b0b858831
teenagegrandma <rebecca.preston90@gmail.com>
2017-11-13 20:36:25 -0600

    changes to server.js

--- server.js
+++ server.js
@@ -1,52 +1,65 @@
+'use strict';
+
 //dependencies
-var express = require('express');
-var path = require('path');
-var mongoose = require('mongoose');
-var expressHandlebars = require('express-handlebars');
-var bodyParser = require('body-parser');
+const express = require('express'),
+	mongoose = require('mongoose'),
+	exphbs = require('express-handlebars'),
+	bodyParser = require('body-parser'),
+	logger = require('morgan'),
+	methodOverride = require('method-override');
+
+const PORT = process.env.PORT || 3000;
 
 var app = express();
 
+//use express
 app.use(express.static(__dirname + '/public'));
 
-//conect handlebars
-app.engine('handlebars', expressHandlebars({
+//connect handlebars
+app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
 //use bodyparser
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+	extended: true
 }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//mongo db directory
-var db = 'mongodb://heroku_6bcvrt0m:7ttugr15dk2s9439ulau4mpmhc@ds157325.mlab.com:57325/heroku_6bcvrt0m';
+//use methodOverride
+app.use(methodOverride('_method'));
 
-//connect to mongoose
-mongoose.connect(db, function(err) {
-	if(err) {
-		console.log(err);
-	}
-	else {
-		console.log('connection successful');
-	}
+//use logger
+app.use(logger('dev'));
+
+//controllers
+app.use(require('./controllers'));
+
+//configure server
+mongoose.Promise = Promise;
+
+const dbURI = process.env.MONGODB_URI || "mongodb://heroku_6bcvrt0m:7ttugr15dk2s9439ulau4mpmhc@ds157325.mlab.com:57325/heroku_6bcvrt0m";
+
+//database config with mongoose
+mongoose.connect(dbURI);
+
+const db = mongoose.connection;
+
+//show errors with mongoose
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
 });
 
-var routes = require('./config/routes.js');
-
-app.use('/', routes);
-app.use('/test', routes);
-app.use('/fetch', routes);
-app.use('/gather', routes);
-app.use('/check', routes);
-app.use('/save', routes);
-app.use('/delete', routes);
-
-//set up port
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_6bcvrt0m:7ttugr15dk2s9439ulau4mpmhc@ds157325.mlab.com:57325/heroku_6bcvrt0m";
-
-//listen on port
-app.listen(port, function() {
-	console.log('listening on port: ' + port);
+//log message if successfully in db through mongoose
+db.once("open", function() {
+	console.log("Successful");
+	//start server, listen on port 3000
+	app.listen(PORT, function() {
+		console.log("App running on port" + PORT);
+	});
 });
+
+//export
+module.exports = app;
